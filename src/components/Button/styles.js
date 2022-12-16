f03608cfe744fb1fffeac465b29b304aef64e3b5
Ivisonj <ivison_j@outlook.com>
2022-12-15 22:35:57 -0300

    Primeiro commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const ButtonContainer = styled.button `
+    padding: 10px;
+   /* border: 1px solid #cdcdcd; */
+    background-color: 	#1C1C1C;
+    color: #ffffff;
+    font-size: 24px;
+    font-weight: 700;
+    flex: 1; 
+    border-radius: 200px ;
+
+    &:hover {
+        opacity: 0.6;
+    }
+
+`
